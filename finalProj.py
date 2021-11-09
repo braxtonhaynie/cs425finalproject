f4f3d5a8ab3c3130d792ed8c1074cc24498554c5
tlovett2 <tlovett2@vols.utk.edu>
2021-11-09 17:38:11 -0500

    Turned images to B&W and got features using yen threshold

--- finalProj.py
+++ finalProj.py
@@ -3,9 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 from skimage import io
+from skimage import filters
+from skimage import restoration
 import pandas as pd
-
+import skimage
 import os
+
+
 
 train_dir = 'DATASET/TRAIN'
 test_dir = 'DATASET/TEST'
@@ -16,15 +20,27 @@ trainim = []
 poses = []
 
 for pose in os.listdir(train_dir):
-    ...
     print(pose)
     currdir = train_dir + '/' + pose
-    poses.append(pose)
 
     for image in os.listdir(currdir):
+        poses.append(pose) #Appends the pose to keep the same order as the images
         im = io.imread(train_dir + '/' + pose + '/' + image)
+        grayIm = skimage.color.rgb2gray(im)
         trainim.append(im)
         print(im)
+        yenned = filters.threshold_yen(grayIm)
+        binary = grayIm > yenned
+        print(binary)
+        newimage = np.ndarray(shape = (binary.shape[0], binary.shape[1], 3), dtype = np.uint8)
+        for row in range(len(binary)):
+          for pixel in range(len(binary[row])):
+            if binary[row][pixel] == True:
+              newimage[row][pixel] = [255, 255, 255]
+            else:
+              newimage[row][pixel] = [0, 0, 0]
+        plt.figure()
+        plt.imshow(newimage)
         break
 
 for ax, im, p in zip(grid, trainim, poses):
