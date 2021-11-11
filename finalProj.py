2ee4853b3398aeebdf87691007987b8bea270a3e
tlovett2 <tlovett2@vols.utk.edu>
2021-11-11 12:46:12 -0500

    added nn. Getting array errors

--- finalProj.py
+++ finalProj.py
@@ -1,3 +1,4 @@
+from itertools import count
 from typing import NoReturn
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,38 +18,99 @@ test_dir = 'DATASET/TEST'
 fig = plt.figure(figsize = (5, 10))
 grid = ImageGrid(fig, 111, nrows_ncols =(5, 1), axes_pad = 0.4)
 trainim = []
-poses = []
+trainposes = []
 
 for pose in os.listdir(train_dir):
-    print(pose)
-    currdir = train_dir + '/' + pose
+  print(pose)
+  currdir = train_dir + '/' + pose
+  i = 0
+  for image in os.listdir(currdir):
+    trainposes.append(pose) #Appends the pose to keep the same order as the images
+    im = io.imread(train_dir + '/' + pose + '/' + image)
+    grayIm = skimage.color.rgb2gray(im)
+    # trainim.append(im)
+    yenned = filters.threshold_yen(grayIm)
+    binary = grayIm > yenned
+    newimage = np.ndarray(shape = (binary.shape[0], binary.shape[1], 3), dtype = np.uint8)
+    nb = []
+    for row in range(len(binary)):
+      # nb = nb.join(binary[row])
+      for pixel in range(len(binary[row])):
+        nb.append(1 if binary[row][pixel] is True else 0)
+      #   if binary[row][pixel] == True:
+      #     newimage[row][pixel] = [255, 255, 255]
+      #   else:
+      #     newimage[row][pixel] = [0, 0, 0]
+    # plt.figure()
+    # plt.imshow(newimage)
 
-    for image in os.listdir(currdir):
-        poses.append(pose) #Appends the pose to keep the same order as the images
-        im = io.imread(train_dir + '/' + pose + '/' + image)
-        grayIm = skimage.color.rgb2gray(im)
-        trainim.append(im)
-        print(im)
-        yenned = filters.threshold_yen(grayIm)
-        binary = grayIm > yenned
-        print(binary)
-        newimage = np.ndarray(shape = (binary.shape[0], binary.shape[1], 3), dtype = np.uint8)
-        for row in range(len(binary)):
-          for pixel in range(len(binary[row])):
-            if binary[row][pixel] == True:
-              newimage[row][pixel] = [255, 255, 255]
-            else:
-              newimage[row][pixel] = [0, 0, 0]
-        plt.figure()
-        plt.imshow(newimage)
-        break
+    trainim.append(nb)
+    if i == 2:
+      break
+    else:
+      i += 1
 
-for ax, im, p in zip(grid, trainim, poses):
-  ax.imshow(im)
-  ax.set_xticks([])
-  ax.set_yticks([])
-  ax.grid(False)
-  ax.set_title(p)
 
-plt.show()
+testim = []
+testposes = []
+for pose in os.listdir(test_dir):
+  print(pose)
+  currdir = train_dir + '/' + pose
+  i = 0
+  for image in os.listdir(currdir):
+    testposes.append(pose)
+    im = io.imread(train_dir + '/' + pose + '/' + image)
+    grayIm = skimage.color.rgb2gray(im)
+    yenned = filters.threshold_yen(grayIm)
+    binary = grayIm > yenned
+    newimage = np.ndarray(shape=(binary.shape[0], binary.shape[1], 3), dtype=np.uint8)
+    nb = []
+    for row in range(len(binary)):
+      # nb = nb.join(binary[row])
+      for pixel in range(len(binary[row])):
+        nb.append(1 if binary[row][pixel] is True else 0)
 
+      #   if binary[row][pixel] == True:
+      #     newimage[row][pixel] = [255, 255, 255]
+      #   else:
+      #     newimage[row][pixel] = [0, 0, 0]
+    # plt.figure()
+    # plt.imshow(newimage)
+
+    testim.append(nb)
+    if i == 2:
+      break
+    else:
+      i += 1
+
+
+# for ax, im, p in zip(grid, trainim, trainposes):
+#   ax.imshow(im)
+#   ax.set_xticks([])
+#   ax.set_yticks([])
+#   ax.grid(False)
+#   ax.set_title(p)
+
+# plt.show()
+
+from sklearn.neural_network import MLPClassifier
+
+net = MLPClassifier()
+print(trainim[1])
+print(trainposes)
+net.fit(trainim, trainposes)
+ypred = net.predict(testim)
+
+count = 0
+correct = 0
+for i in range(len(ypred)):
+  if ypred[i] == testposes[i]:
+    print('Correct: {}'.format(testposes[i]))
+    correct += 1
+  else:
+    print('Incorrect: Guessed {}, Correct: {}'.format(ypred[i], testposes[i]))
+  count += 1
+
+print()
+print('Accuracy: {}'.format(correct/count))
+print("{} / {}".format(correct, count))
